f90583df70ab4e5ef8c21c8b77b2164ee5c6d5a8
GOSASCO <gosasco92@gmail.com>
2022-07-04 23:48:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,28 +12,31 @@ int main(void)
 	int a = '0';
 	int b = '0';
 
-	while (b <= '9')
+	while (a <= '9')
 	{
 		while (b <= '9')
 		{
-			if (!(a < b || a == b))
+			if (!(a > b || a == b))
 			{
 				putchar(a);
 				putchar(b);
 
 				if (a == '8' && b == '9')
+				{
 					putchar('\n');
+				}
 				else
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				b++;
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			b = '0';
-			a++;
+			b++;
 		}
+		b = '0';
+		a++;
 	}
+
 	return (0);
 }
 
