79ab28f9e1cc631eb3c764db4ac19191589a2ee4
GOSASCO <gosasco92@gmail.com>
2022-07-06 09:46:25 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,20 +14,22 @@ int main(void)
 	{
 		for (b = a + 1; a <= 99; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar('\n');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-			if (a == 98 || b == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
-			
+			if (a < b && a != b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(',');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 
 	putchar('\n');
-
 	return (0);
 }
