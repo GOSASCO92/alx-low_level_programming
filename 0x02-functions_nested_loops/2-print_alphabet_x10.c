9945aa0bd9bffd9a790f765fd98bbd1f2ef87dee
GOSASCO <gosasco92@gmail.com>
2022-07-07 14:34:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,8 @@ void print_alphabet_x10(void)
 	char alph;
 	int i;
 
-	for (i = 0; i <= 10; ++i)
+	for (i = 0; i <= 10; i++)
 		for (alph = 'a'; alph <= 'z'; alph++)
 			_putchar(alph);
-	_putchar('\n');
+		_putchar('\n');
 }
