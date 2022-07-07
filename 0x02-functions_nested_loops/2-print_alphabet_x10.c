195e923e3025d1dcefd511f7c142889bf77bce9f
GOSASCO <gosasco92@gmail.com>
2022-07-07 14:29:35 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+
+	for (i = 0; i <= 10; ++i)
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph);
+	_putchar('\n');
+}
