d0e4c8a9a97dc0f528cc408f14dc2c3bb5a824d2
GOSASCO <gosasco92@gmail.com>
2022-07-07 13:52:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints alphabets in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+}
