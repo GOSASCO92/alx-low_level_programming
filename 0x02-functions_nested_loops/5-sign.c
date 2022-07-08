5d04e460a528d4cfbc5af900a9b14a8d1913157d
GOSASCO <gosasco92@gmail.com>
2022-07-08 11:57:22 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sigin - prints the sign of a number
+ *
+ * @n: number to check
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+		_putchar('\n');
+}
