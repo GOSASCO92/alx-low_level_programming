0887842acffc5d4207413ceb361388839aba0bcc
GOSASCO <gosasco92@gmail.com>
2022-07-08 14:00:17 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if ((c % 10) == 0)
+			if ((c / 10) == 0)
 			{
 				if (b != 0)
 					_putchar(' ');
@@ -27,8 +27,8 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((c / 10));
-				_putchar((c % 10));
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 
 				if (b == 9)
 					continue;
