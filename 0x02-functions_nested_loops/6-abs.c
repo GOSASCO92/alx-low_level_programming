9f829d3047d68dbd64490e2202efe6a4d63f6256
GOSASCO <gosasco92@gmail.com>
2022-07-08 12:40:09 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of a number
+ *
+ * @n: number to be checked
+ * Return: result
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n *= -1;
+	return (n);
+}
