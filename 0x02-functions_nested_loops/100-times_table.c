a9f808995db41a96c57e59f76ee877bce39cb17f
GOSASCO <gosasco92@gmail.com>
2022-07-09 00:39:36 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints  times table
+ * @n: number to print
+ */
+
+void print_times_table(int n)
+{
+	int i, x;
+
+	if (!(n > 15 || n == 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(0);
+			for (x = 1; x <= 15; x++)
+			{
+				if (n <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(n + '0');
+				}
+				else if (n > 9 && n <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((n / 10) + '0');
+					_putchar((n / 10 % 10) + '0');
+					_putchar((n % 10) + '0');
+				}
+				_putchar('\n');
+			}
+		}
+	}
+}
