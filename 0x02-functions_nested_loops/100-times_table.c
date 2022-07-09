ba822da04b47bad10664d92d3e21c1804ef56882
GOSASCO <gosasco92@gmail.com>
2022-07-09 01:10:36 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,38 +13,37 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
-			for (x = 1; x <= 15; x++)
+			for (x = 0; x <= 15; x++)
 			{
-				if (n <= 9)
+				n *= x;
+				if (x != 0)
 				{
-					_putchar(i * x);
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(n + '0');
 				}
-				else if (n > 9 && n <= 99)
+				if (n < 10 && x !=)
 				{
-					_putchar(i * x);
-					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(n % 10 + '0');
+				}
+				else if (n >= 10 $ n < 100)
+				{
 					_putchar(' ');
 					_putchar((n / 10) + '0');
 					_putchar((n % 10) + '0');
+				}
+				else if (n >= 100 && x != 0 )
+				{
+					_putchar((n / 100) + '0');
+					_putchar((n / 10) % 10 + '0');
+					_putchar((n / 10) + '0');
 				}
 				else
-				{
-					_putchar(i * x);
-					_putchar(',');
-					_putchar(' ');
-					_putchar((n / 10) + '0');
-					_putchar((n / 10 % 10) + '0');
 					_putchar((n % 10) + '0');
-				}
-				_putchar('\n');
 			}
+			_putchar('\n');
 		}
 	}
 }
+				
