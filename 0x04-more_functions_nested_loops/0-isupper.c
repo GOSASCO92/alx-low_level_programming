e81ea2f95bde0c62ef96f16d70b4ab054ccf4d0e
GOSASCO <gosasco92@gmail.com>
2022-07-11 12:30:09 -0700

    0x04 project

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the case of a character
+ * @c: character to be checked
+ */
+
+int _isupper(int c)
+{
+	char upper = 'A';
+
+	for (; upper <= 'Z'; upper++)
+	{
+		if (c == upper)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
