cc9272f0ebcf41743c5b021d4741b56e28942573
GOSASCO <gosasco92@gmail.com>
2022-07-12 01:43:38 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,14 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (; i < n; i++)
-		       _putchar('_');
+		_putchar('\n');
 	}
-	_putchar('_');
+	else
+	{
+		for (; i <= n; i++)
+			_putchar('_');
+	}
 }
 
