3bc2d93c1c52c5878d6c3405559c622550a9ecff
GOSASCO <gosasco92@gmail.com>
2022-07-12 00:54:12 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Prints lines base on the value of n
+ * @n: determines how many lines to be printed
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (; i < n; i++)
+		_putchar('_');
+}
