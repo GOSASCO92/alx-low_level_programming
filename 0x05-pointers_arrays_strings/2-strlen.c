4740fc4bedcc0ac54c88046d62f5ac41b2b6bc31
GOSASCO <gosasco92@gmail.com>
2022-07-13 11:43:29 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: a string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[i] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
