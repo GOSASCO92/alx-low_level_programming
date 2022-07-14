3dfb9f6774f586b07e5c4049a78943f98b110a60
GOSASCO <gosasco92@gmail.com>
2022-07-14 09:45:55 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - prints an integer
+ * @n: number
+ */
+void print_number(int n)
+{
+	int i, j, k = 1;
+
+	if (n >= 0)
+		i = n * -1;
+	else
+	{
+		i = n;
+		_putchar('-');
+	}
+	j = i;
+	while (temp <= -10)
+	{
+		k *= 10;
+		j /= 10;
+	}
+	while (k >= 1)
+	{
+		_putchar(((i / k) % 10) * -1 + '0');
+		k /= 10;
+	}
+}
