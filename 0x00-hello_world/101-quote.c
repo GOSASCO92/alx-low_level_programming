fdc8d6aece6c98e2ece9a845e2e8b527f0c7cd75
GOSASCO <gosasco92@gmail.com>
2022-12-09 20:00:20 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 1
+ */
+int main(void)
+{
+write(STDOUT_FILENO, "and that piece art is useful
+\" - Dora Karpar, 2015-10-19\n", 59);
+return (1);
+}
