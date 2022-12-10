559202088cd73d36ae0e2cdfdc89bff0df71887b
GOSASCO <gosasco92@gmail.com>
2022-12-09 20:09:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece art is useful
+fwrite(STDOUT_FILENO, "and that piece art is useful
 \" - Dora Karpar, 2015-10-19\n", 59);
 return (1);
 }
