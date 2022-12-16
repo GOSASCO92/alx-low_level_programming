3b7c1ad952b87e391d7886b3681b5027def0e5d7
GOSASCO <gosasco92@gmail.com>
2022-12-16 01:02:29 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = '1'; i < '100'; i++)
+{
+if (i <= '9')
+{
+putchar('0');
+if (i != '99')
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
