5d7d2d308ffc6412fe855f9b8d0c374790e9ab19
GOSASCO <gosasco92@gmail.com>
2022-12-16 02:26:50 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: 0
+ */
+int main(void)
+{
+int i, j, k;
+for (i = '0'; i < '9'; i++)
+{
+for (j = d + 1; j <= '9'; j++)
+{
+for (k = j + 1; j <= '9'; j++)
+{
+if ((j != i) != k)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i == '7' && j == '8')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
