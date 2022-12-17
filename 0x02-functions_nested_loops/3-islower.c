b47f35e30f5ef735d47a7f879d88ee7a2d5139a9
GOSASCO <gosasco92@gmail.com>
2022-12-17 03:06:55 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - checks if case is lower
+ *@c: user input
+ *Return: 1 if true, 0 otherwise
+ */
+int _islower(int c)
+{
+char a;
+int i;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a == i)
+i = 1;
+}
+return (i);
+}
