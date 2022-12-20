3b66dc7b9844d4c776f4684b239b64e24d3da956
GOSASCO <gosasco92@gmail.com>
2022-12-20 01:01:44 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,7 @@
 void print_to_98(int n)
 {
 if (n <= 98)
+{
 for (; n <= 98; n++)
 {
 printf("%d", n);
@@ -16,7 +17,9 @@ continue;
 printf(", ");
 }
 printf("\n");
+}
 else
+{
 for (; n >= 98; n--)
 {
 printf("%d", n);
@@ -25,4 +28,5 @@ continue;
 printf(", ");
 }
 printf("\n");
+}
 }
