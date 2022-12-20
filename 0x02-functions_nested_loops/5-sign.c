4678cd8b26ddce831d0e48d63181951c111dbd58
GOSASCO <gosasco92@gmail.com>
2022-12-19 22:58:20 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  *print_sign - checks the sign of a number
- *n: user input
+ *@n: user input
  *Return: 1, 0, -1 based is check
  */
 int print_sign(int n)
 {
 int result;
-  
+
 if (n > 0)
 {
 result = 1;
