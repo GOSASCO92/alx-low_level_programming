bc0d5d65b770bb45f333ebb3055c9a642b1c1eb8
GOSASCO <gosasco92@gmail.com>
2022-12-20 02:39:20 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: 0
+ */
+int main(void)
+{
+int x, y;
+
+for (x = 1; x < 1024; x++)
+{
+if (x % 3 == 0 || x % 5 == 0)
+y += x;
+}
+return (y);
+}
