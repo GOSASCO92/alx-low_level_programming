624c65afdbf0a9e4d4dc8a1926adb77cdf2e3111
GOSASCO <gosasco92@gmail.com>
2022-12-20 00:07:30 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 k = i * j;
-if ((k % 10) == 0)
+if ((k / 10) == 0)
 {
 if (j != 0)
 _putchar(' ');
@@ -24,8 +24,8 @@ _putchar(' ');
 }
 else
 {
-_putchar(k / 10);
-_putchar(k % 10);
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 if (j == 9)
 continue;
 _putchar(',');
