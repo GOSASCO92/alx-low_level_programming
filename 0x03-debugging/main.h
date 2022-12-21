fa779c0e4e68097e0fa456f42d8b3866f81de867
GOSASCO <gosasco92@gmail.com>
2022-12-21 10:02:26 -0800

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,4 @@
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
-int print_remaining_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
