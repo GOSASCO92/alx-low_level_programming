bb85696e0952d923c187608a78d7363965b5dd80
GOSASCO <gosasco92@gmail.com>
2022-12-21 06:55:18 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+long int f;
+
+f[0] = 1;
+f[1] = 2;
+printf("%ld, %d, ", f[0], f[1]);
+for (i = 2; i < 50; i++)
+{
+f[i] = f[i - 1] + f[i - 2];
+if (i == 49)
+printf("%ld\n", f[i]);
+else
+printf("%ld, ", f[i]);
+}
+return (0);
+}
