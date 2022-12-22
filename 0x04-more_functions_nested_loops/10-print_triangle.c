d43b878574bffa47381bb5e4810504129eeb7b08
GOSASCO <gosasco92@gmail.com>
2022-12-22 06:15:06 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_triangle - prints a triangular shape
+ *@size: size of the triangle
+ *Return: void
+ */
+
+void print_triangle(int size)
+{
+int i, j, k = size - 1;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (j < k)
+_putchar(' ');
+else
+_putchar('#');
+}
+k--;
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
