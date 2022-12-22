597dba93e1935d60d7115de521d70ab663fa30b9
GOSASCO <gosasco92@gmail.com>
2022-12-22 06:29:58 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,9 +9,9 @@ unsigned long int i, j = 612852475143;
 
 for (i = 3; i < 12057; i += 2)
 {
-while (j % i == 0 && k != i)
-k /= i;
+while (j % i == 0 && j != i)
+j /= i;
 }
-printf("%lu\n", k);
+printf("%lu\n", j);
 return (0);
 }
