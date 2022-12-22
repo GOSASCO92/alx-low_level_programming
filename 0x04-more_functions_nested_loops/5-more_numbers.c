0e7d4632832acc3bb4b3af529285e43781f9d2e6
GOSASCO <gosasco92@gmail.com>
2022-12-22 03:47:19 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *more_numbers - print times 10 of number 0 -14
+ *return: 0
+ */
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 0; i <= 14; i++)
+{
+for (j = '0'; j <= 14; j++)
+_putchar(j);
+_putchar('\n');
+}
+_putchar('\n');
+}
