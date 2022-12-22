7e912b64627c145b1170557e09e7fb5b44e41dcd
GOSASCO <gosasco92@gmail.com>
2022-12-22 06:45:08 -0800

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *print_number - prints and integer
+ *@n: input
+ *Return: 0
+ */
+void print_number(int n)
+{
+if (n == 0)
+_putchar('0');
+else if (n < 0)
+{
+_putchar('-');
+print_int(n * -1);
+}
+else
+print_int(n);
+}
+
+/**
+ *print_int - prints n
+ *@c: input
+ *Return: 0
+ */
+void print_int(int c)
+{
+int i = 1000000000;
+
+for (; i >= 1; i /= 10)
+if (c / i != 0)
+_putchar(c / i) % 10 + '0');
+}
