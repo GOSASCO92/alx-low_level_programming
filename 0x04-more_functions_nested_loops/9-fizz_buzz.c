a498761e0f217bf7fc22e94d98fee924308ebbba
GOSASCO <gosasco92@gmail.com>
2022-12-22 05:56:00 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <= 100; i++)
+for (i = 1; i < 100; i++)
 {
 if (i % 3 == 0)
 printf("Fizz ");
@@ -15,10 +15,9 @@ else if (i % 5 == 0)
 printf("Buzz ");
 else if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz ");
-if (x == 100)
-printf("%d", i);
 else
 printf("%d ", i);
 }
-printf("\n");
+printf("Buzz\n");
+return (0);
 }
