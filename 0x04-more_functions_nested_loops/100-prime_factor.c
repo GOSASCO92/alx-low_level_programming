2a9914fe435ad08ec988d27dd4d75a689e5ae530
GOSASCO <gosasco92@gmail.com>
2022-12-22 06:26:34 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: 0
+ */
+int main(void)
+{
+unsigned long int i, j = 612852475143;
+
+for (i = 3; i < 12057; i += 2)
+{
+while (j % i == 0 && k != i)
+k /= i;
+}
+printf("%lu\n", k);
+return (0);
+}
