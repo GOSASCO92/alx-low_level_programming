be3eadb89b0d27b5d972f83bf82f7d2b4a2c8479
GOSASCO <gosasco92@gmail.com>
2023-01-10 06:36:45 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_atoi - converts string to integer
+ *@s: input string
+ *Return: 0
+ */
+int _atoi(char *s)
+{
+int i = 1;
+unsigned int t = 0;
+char f = 0;
+
+while (*s)
+{
+if (*s == '-')
+i = -1;
+if (*s >= '0' && *s <= '9')
+{
+f = 1;
+t = t * 10 + *s - '0';
+}
+else if (f)
+break;
+s++;
+}
+if (i < 0)
+t -= t;
+return (t);
+}
